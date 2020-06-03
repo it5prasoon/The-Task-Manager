49d23a52466861d5fee01628463c8b740c9f03c3
Prasoon <it5prasoon@users.noreply.github.com>
2020-06-03 12:51:23 +0530

    Update mongodb.js

--- mongodb.js
+++ mongodb.js
@@ -1,10 +1,17 @@
 // CRUD create read update delete
 
-const mongodb = require('mongodb')
-const MongoClient = mongodb.MongoClient
+// const mongodb = require('mongodb')
+// const MongoClient = mongodb.MongoClient
+// const ObjectID = mongodb.ObjectID
+
+const { MongoClient, ObjectID } = require('mongodb')
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
+
+const id = new ObjectID()
+console.log(id.id.length)
+console.log(id.toHexString().length)
 
 MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
     if (error) {
@@ -13,30 +20,5 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
 
     const db = client.db(databaseName)
     
-    // db.collection('users').insertOne({
-    //     name: 'Prasoon',
-    //     age: 19
-    // }, (error, result) => {
-    //     if (error) {
-    //         return console.log('Unable to insert User')
-    //     }
-
-    //     console.log(result.ops)
-    // })
-
-    db.collection('users').insertMany([
-        {
-            name: 'Guggu',
-            age: 19
-        }
-        , {
-            name: 'Guggi',
-            age: '17'
-        }], (error, result) => {
-            if(error) {
-                return console.log('Unable to insert1')
-            }
-
-            console.log(result.ops)
-        })
+   
 })
