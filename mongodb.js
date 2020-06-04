e36636bf661639d93b874753df2c0a9b448ee691
Prasoon <it5prasoon@users.noreply.github.com>
2020-06-04 08:59:13 +0530

    added findOne and updateOne in monogDB.js

--- mongodb.js
+++ mongodb.js
@@ -10,8 +10,8 @@ const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
 
 const id = new ObjectID()
-console.log(id.id.length)
-console.log(id.toHexString().length)
+// console.log(id.id.length)
+// console.log(id.toHexString().length)
 
 MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
     if (error) {
@@ -20,5 +20,24 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
 
     const db = client.db(databaseName)
     
-   
+    // db.collection('users').findOne({name: 'Prasoon'}, (error, user) => {
+    //     if (error) {
+    //         return console.log('unable to fetch!')
+    //     }
+
+    //     console.log(user)
+    // })
+    const updatePromise = db.collection('users').updateOne({
+        _id: new ObjectID("5ed711d1c2d0664570ec63a2")
+    }, {
+        $inc: {
+            age: 1
+        }
+    })
+
+    updatePromise.then((result) => {
+        console.log(result)
+    }).catch((error) => {
+        console.log(error)
+    })
 })
