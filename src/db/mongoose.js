ef25f66750a7fdf9a07499466916d9687031f8dc
Prasoon <it5prasoon@users.noreply.github.com>
2020-06-04 14:42:24 +0530

    added validator and models for user, email etc

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
+
+
 mongoose.connect('mongodb://localhost:27017/task-manager-api', {
     useNewUrlParser: true,
     useCreateIndex: true
@@ -6,40 +9,59 @@ mongoose.connect('mongodb://localhost:27017/task-manager-api', {
 
 const User = mongoose.model ('User', {
     name: {
-        type: String
+        type: String,
+        required: true, 
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is Invalid')
+            }
+        }
     },
     age: {
-        type: Number
+        type: Number,
+        default: 0,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Age must be a positive number!')
+            }
+        }
     }
 })
 
-// const me = new User({
-//     name: 'Prasoon',
-//     age: 19
-// })
-
-// me.save().then(() => {
-//     console.log(me)
-// }).catch((error) => {
-//     console.log('Error!', error)
-// })
-
-const Task = mongoose.model('Task', {
-    description: {
-        type: String
-    }, 
-    completed: {
-        type: Boolean
-    }
+const me = new User({
+    name: 'Prasoon',
+    email: 'mike@gmail.com'
 })
 
-const task = new Task({
-    description: 'Learn the mongoose library',
-    completed: true
-})
-
-task.save().then(() => {
-    console.log(task)
+me.save().then(() => {
+    console.log(me)
 }).catch((error) => {
-    console.log(error)
-    })
+    console.log('Error!', error)
+})
+
+// const Task = mongoose.model('Task', {
+//     description: {
+//         type: String
+//     }, 
+//     completed: {
+//         type: Boolean
+//     }
+// })
+
+// const task = new Task({
+//     description: 'Learn the mongoose library',
+//     completed: true
+// })
+
+// task.save().then(() => {
+//     console.log(task)
+// }).catch((error) => {
+//     console.log(error)
+//     })
